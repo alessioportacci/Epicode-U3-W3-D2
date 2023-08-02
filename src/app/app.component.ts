36f11ed534b6d6cd388582bab06deb95204fe3d6
alessioportacci <alessio.portacci.s@gmail.com>
2023-08-02 22:30:16 +0200

    Esercizio concluso

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Epicode-U3-W3-D2';
+export class AppComponent implements OnInit {
+  formApp!: FormGroup;
+
+  constructor(){}
+  ngOnInit(): void {
+    this.formApp = new FormGroup
+    ({
+      nome: new FormControl("", [Validators.required, Validators.minLength(3)]),
+      cognome: new FormControl(null, [Validators.required, Validators.minLength(3)]),
+      password: new FormControl(null, [Validators.required, Validators.minLength(5)]),
+      cPassword: new FormControl(null, [Validators.required, Validators.minLength(5)]),
+      genere: new FormControl(),
+      img: new FormControl(),
+      bio: new FormControl(),
+      username: new FormControl(null, [Validators.required, Validators.minLength(3)]),
+    })  }
+
+  onSubmit()
+  {
+    console.log(this.formApp)
+  }
+
 }
